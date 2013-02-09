e8fe19af6d6debfadcb20436513ded298f9048c7
Morten Lied Johansen <mortenjo@ifi.uio.no>
2013-02-09 22:53:32 +0100

    Fixes and debugging

--- tv/next.php
+++ tv/next.php
@@ -3,12 +3,28 @@
     define("NEXT_PATTERN", "next$[0-9]+x[0-9]+.py");
 
     function scan_for_file($root, $target) {
+        echo "Scanning dir: " . $root . "\n";
+
         $files = scandir($root);
+
+        echo "Files: \n";
+        print_r($files);
+
         if (in_array($target, $files)) {
             return $root;
         }
+
+        echo "Not found, scanning subdirs\n";
+
         $dirs = array_filter($files, "is_dir");
+
+        echo "Dirs: \n";
+        print_r($dirs);
+
         foreach ($dirs as $dir) {
+            if ($dir[0] == ".") {
+                continue;
+            }
             $result = scan_for_file($dir, $target);
             if (!is_null($result)) {
                 return $result;
@@ -26,10 +42,14 @@
         return NULL;
     }
 
-    $file = urldecode($_GET["next"]);
+    $file = $_GET["next"];
     $season_dir = scan_for_file(SERIES_ROOT, $file);
-    $series_dir = dirname($season_dir);
-    $next_file = find_next_file($series_dir);
-    system("python " . $next_file);
+    if (is_null($season_dir)) {
+        echo "Failed to find file: " . $file . "\n";
+    } else {
+        $series_dir = dirname($season_dir);
+        $next_file = find_next_file($series_dir);
+        system("python " . $next_file);
 
-    header("index.php", 301);
+        header("index.php", 301);
+    }
