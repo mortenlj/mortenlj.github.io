0aa76e8e958a837ef14a22f38aebdbf713322208
Morten Lied Johansen <mortenjo@ifi.uio.no>
2013-02-11 22:39:17 +0100

    Some bugfixes

--- tv/next.php
+++ tv/next.php
@@ -1,8 +1,8 @@
 <?php
     define("SERIES_ROOT", "/volume1/video/series");
     define("NEW_DIR", join_dir(SERIES_ROOT, "0-NEW-0"));
-    define("REFRESH_MARKER", join_dir(SERIES_ROOT, ".needs_refresh"));
-    define("NEXT_PATTERN", "next$([0-9]+)x([0-9]+).py");
+    define("REFRESH_MARKER", join_dir("/tmp", "series_needs_refresh"));
+    define("NEXT_PATTERN", "next\\$([0-9]+)x([0-9]+)\\.py");
 
     function join_dir($root, $file) {
         return $root . DIRECTORY_SEPARATOR . $file;
@@ -39,7 +39,7 @@
 
             foreach ($dirs as $dir) {
                 $subdirs = list_dirs($dir);
-                foreach ($subdirs as $$subdir) {
+                foreach ($subdirs as $subdir) {
                     if (file_in_dir($file, $subdir)) {
                         return $dir;
                     }
@@ -51,7 +51,7 @@
 
     function find_next_match($dir) {
         $files = scandir($dir);
-        foreach ($files as $$file) {
+        foreach ($files as $file) {
             $match = array();
             if (preg_match(NEXT_PATTERN, $file, $match)) {
                 return $match;
@@ -65,7 +65,7 @@
     if (!is_null($series_dir)) {
         $match = find_next_match($series_dir);
         $old_name = join_dir($series_dir, $match[0]);
-        $new_name = join_dir($series_dir, sprintf("next$\02dx\02d.py", $match[1], $match[2] + 1));
+        $new_name = join_dir($series_dir, sprintf("next$%dx%02d.py", $match[1], $match[2] + 1));
         rename($old_name, $new_name);
         touch(REFRESH_MARKER);
     }
